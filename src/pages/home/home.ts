2bc131bca051c1957070b9c88ec2d43498afc5b9
Irene Camunag <icamunag@apprioinc.com>
2018-07-27 14:22:15 -0400

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { TemplatePage } from '../template/template';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  username:string;
+  password:string;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login(){
+    /*
+    console.log("Username: " + this.username);
+
+    console.log("Password: " + this.password);
+    **/
+    this.navCtrl.push(TemplatePage);
   }
 
 }
