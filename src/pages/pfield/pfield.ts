1deebc3ee1f4268d4bdefd9754d8312a198c9f97
Irene Camunag <icamunag@apprioinc.com>
2018-09-10 13:39:47 -0400

    Created a button for subtemplates and moved into Patient template

--- src/pages/pfield/pfield.ts
+++ src/pages/pfield/pfield.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CameraPage } from '../../pages/camera/camera';
+import { PaymentPage } from '../../pages/payment/payment';
+import { ScreeningPage } from '../../pages/screening/screening';
+import { NotePage } from '../../pages/note/note';
 
 /**
  * Generated class for the PfieldPage page.
@@ -20,6 +24,22 @@ export class PfieldPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PfieldPage');
+  }
+
+  CameraPage() {
+    this.navCtrl.push(CameraPage);
+  }
+
+  PaymentPage() {
+    this.navCtrl.push(PaymentPage);
+  }
+
+  ScreeningPage() {
+    this.navCtrl.push(ScreeningPage);
+  }
+
+  NotePage() {
+    this.navCtrl.push(NotePage);
   }
 
 }
